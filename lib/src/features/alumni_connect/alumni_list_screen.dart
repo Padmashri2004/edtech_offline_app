54ad10991fa2e6e73f208c7353ec02734c11f28c
revthree <revthree>
2026-02-07 14:06:21 +0530

    feat: Integrate Alumni list screen

--- lib/src/features/alumni_connect/alumni_list_screen.dart
+++ lib/src/features/alumni_connect/alumni_list_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'models/alumni_model.dart';
 import 'alumni_profile_screen.dart';
+import '../student_tracking/connection_service.dart';
+
 
 class AlumniListScreen extends StatelessWidget {
   final List<AlumniModel> alumni = [
@@ -20,20 +22,33 @@ class AlumniListScreen extends StatelessWidget {
         itemCount: alumni.length,
         itemBuilder: (context, index) {
           final a = alumni[index];
-          return ListTile(
-            leading: const CircleAvatar(child: Icon(Icons.person)),
-            title: Text(a.name),
-            subtitle: Text(a.profileId),
-            trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => AlumniProfileScreen(alumni: a),
-                ),
-              );
-            },
-          );
+        return ListTile(
+  leading: const CircleAvatar(child: Icon(Icons.person)),
+  title: Text(a.name),
+  subtitle: Text(a.profileId),
+  trailing: IconButton(
+    icon: const Icon(Icons.person_add),
+    onPressed: () async {
+      await ConnectionService().requestMentorship(
+        studentId: 'student_001',
+        alumniId: a.profileId,
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Mentorship request sent")),
+      );
+    },
+  ),
+  onTap: () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => AlumniProfileScreen(alumni: a),
+      ),
+    );
+  },
+);
+
         },
       ),
     );
